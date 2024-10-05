25a6b969a101996d230c2031081cd5d318be8353
BentlyM <metayerbently@gmail.com>
2024-10-04 23:53:01 -0400

    refactored reader oasis to have correct context based on json data...

--- src/pages/CardDetails.tsx
+++ src/pages/CardDetails.tsx
@@ -5,14 +5,11 @@ import { useAuth } from '../hooks/AuthProvider';
 
 interface Comment {
   id: number;
+  user: { username: string };
   content: string;
   createdAt: string;
   postId: number;
   userId: number;
-  post: Post
-  user: {
-    username: string;
-  };
 }
 
 interface Post {
@@ -24,15 +21,13 @@ interface Post {
   updatedAt: string;
   authorId: number;
   comments: Comment[];
-  msg: string
 }
 
-
-const CardDetails = () => {
+const CardDetails: React.FC = () => {
   const { id } = useParams();
   const auth = useAuth();
 
-  const [post, setPost] = useState<Post>();
+  const [post, setPost] = useState<Post | null>(null);
   const [loading, setLoading] = useState(true);
   const [err, setErr] = useState<string | null>(null);
   const [comment, setComment] = useState('');
@@ -41,7 +36,7 @@ const CardDetails = () => {
     (async () => {
       try {
         const res = await fetch(
-          `https://backend-uoiu.onrender.com/api/posts/${id}/comments`
+          `https://backend-uoiu.onrender.com/api/posts/${id}`
         );
         if (!res.ok) throw new Error('Error fetching data');
 
@@ -73,29 +68,25 @@ const CardDetails = () => {
             Accept: 'application/json',
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            content: comment,
-          }),
+          body: JSON.stringify({ content: comment }),
         }
-      ); 
+      );
 
       target.content.value = '';
-      
       setComment(comment);
     } catch (e) {
       console.error(e);
     }
   };
 
-  function commentCards() {
-    return post!.comments.map((comment, index) => (
+  const commentCards = () => {
+    return post?.comments.map((comment, index) => (
       <div
         key={index}
         className="commentCard"
         style={{
           backgroundColor: 'white',
           border: '1px solid gray',
-          borderImage: 'liner-gradient(to right, #b4b4b4 20%, #fff) 1',
           padding: '8px',
           display: 'flex',
           flexDirection: 'column',
@@ -103,129 +94,111 @@ const CardDetails = () => {
       >
         <span>{comment.content}</span>
         <span className="signature">
-          -by <strong>{comment.user?.username || 'anonymous'}</strong> on {formatDate(comment.createdAt)}
+          -by <strong>{comment.user.username || 'anonymous'}</strong> on{' '}
+          {formatDate(comment.createdAt)}
         </span>
       </div>
     ));
-  }
+  };
 
   if (loading) return <div>Loading...</div>;
   if (err) return <div>{err}</div>;
 
   return (
-    <>
-      {post?.msg != 'Comments Found' ? (
+    <div>
+      {!post ? (
         <div>Post Does Not Exist...</div>
       ) : (
-        <div>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <div
             style={{
               display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              flexDirection: 'column',
+              minWidth: '700px',
             }}
           >
-            <div
+            <h2>Post Details</h2>
+            <h3
               style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'start',
-                flexDirection: 'column',
-                minWidth: '700px',
+                backgroundColor: '#4ade80',
+                color: '#fff',
+                marginBottom: '8px',
+                marginTop: '0px',
+                width: '100%',
               }}
             >
-              <h2>Post Details</h2>
-              <>
-                <h3
-                  style={{
-                    backgroundColor: '#4ade80',
-                    boxShadow: '2px 10px 0 -7px green',
-                    color: '#fff',
-                    marginBottom: '8px',
-                    marginTop: '0px',
-                    width: '100%',
-                  }}
-                >
-                  {post?.comments[0].post.title}
-                </h3>
-                <p>{post?.comments[0].post.content}</p>
-                <p className="signature">
-                  -by <strong>{post?.comments[0].user.username || 'anonymous'}</strong> on {formatDate(post!.comments[0].post.createdAt)}
-                </p>
-                <div className="comments" style={{ width: '100%' }}>
-                  <h3
-                    style={{
-                      backgroundColor: '#4ade80',
-                      boxShadow: '2px 10px 0 -7px green',
-                      color: '#fff',
-                      marginBottom: '8px',
-                      marginTop: '0px',
-                      width: '100%',
-                    }}
-                  >
-                    Comments:{' '}
-                  </h3>
-                  {post!.comments.length > 0 ? (
-                    commentCards()
-                  ) : (
-                    <p>No comments...</p>
-                  )}
-                </div>
-
-                <form
-                  className="commentForm"
-                  action=""
-                  onSubmit={handleComment}
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    flexDirection: 'column',
-                    width: '100%',
-                  }}
-                >
-                  <div
-                    style={{
-                      display: 'flex',
-                      maxWidth: 'none',
-                      width: 'auto',
-                      flexDirection: 'column',
-                    }}
-                  >
-                    <label htmlFor="content">Comment: </label>
-                    <textarea
-                      name="content"
-                      required
-                      maxLength={150}
-                      minLength={3}
-                      style={{
-                        resize: 'vertical',
-                        fontSize: 'inherit',
-                        padding: '8px',
-                        height: '107px',
-                      }}
-                      title={
-                        auth.isAuthenticated
-                          ? ''
-                          : 'Authenticate to post comments'
-                      }
-                      disabled={auth.isAuthenticated ? false : true}
-                    ></textarea>
-                  </div>
-                  <button
-                    type="submit"
-                    disabled={auth.isAuthenticated ? false : true}
-                    style={{ width: 'fit-content' }}
-                  >
-                    {auth.isAuthenticated ? 'Submit' : 'Log in to comment'}
-                  </button>
-                </form>
-                <div className="errors"></div>
-              </>
+              {post.title}
+            </h3>
+            <p>{post.content}</p>
+            <p className="signature">
+              -by{' '}
+              <strong>{post.comments[0]?.user.username || 'anonymous'}</strong>{' '}
+              on {formatDate(post.createdAt)}
+            </p>
+            <div className="comments" style={{ width: '100%' }}>
+              <h3
+                style={{
+                  backgroundColor: '#4ade80',
+                  color: '#fff',
+                  marginBottom: '8px',
+                  marginTop: '0px',
+                  width: '100%',
+                }}
+              >
+                Comments:
+              </h3>
+              {post.comments.length > 0 ? (
+                commentCards()
+              ) : (
+                <p>No comments...</p>
+              )}
             </div>
+
+            <form
+              className="commentForm"
+              onSubmit={handleComment}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                width: '100%',
+              }}
+            >
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <label htmlFor="content">Comment:</label>
+                <textarea
+                  name="content"
+                  required
+                  maxLength={150}
+                  minLength={3}
+                  style={{
+                    resize: 'vertical',
+                    padding: '8px',
+                    height: '107px',
+                  }}
+                  title={
+                    !auth.isAuthenticated ? 'Authenticate to post comments' : ''
+                  }
+                  disabled={!auth.isAuthenticated}
+                />
+              </div>
+              <button
+                type="submit"
+                disabled={!auth.isAuthenticated}
+                style={{ width: 'fit-content' }}
+              >
+                {auth.isAuthenticated ? 'Submit' : 'Log in to comment'}
+              </button>
+            </form>
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
